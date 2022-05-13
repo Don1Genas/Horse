185e3ae6a06c3d45251e0cbacde4662621a0168e
Donavan Genas <donavangenas@gmail.com>
2022-05-13 12:55:39 -0400

    second commit on day four

--- horse.js
+++ horse.js
@@ -2,13 +2,12 @@
 //trigger coin toss
 function toss() {
     const coin = document.getElementById("coin");
-    coin.style.animation = "none";
+    // coin.style.animation = "none";
     if (Math.random() <= 0.5) {
-      coin.style.animation = "flip-heads 3s forwards";
-      //alert ("Player 1 goes first!")
+    //   coin.style.animation = "flip-heads 3s forwards";
+      alert ("Player 1 goes first!")
     } else {
-      coin.style.animation = "flip-tails 3s forwards";
-      //alert ("Player 2 goes first!")
+    //   coin.style.animation = "flip-tails 3s forwards";
+    alert ("Player 2 goes first!")
     }
   }
-  
