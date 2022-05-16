f4210fc50e75cb6fc6f9dd0b47aec37bc346df34
Donavan Genas <donavangenas@gmail.com>
2022-05-16 09:05:17 -0400

    first commit of day six

--- horse.js
+++ horse.js
@@ -14,12 +14,31 @@ function toss() {
   }
 
   //Choice of shots
-let dunk1 = document.getElementById("windmill");
-let dunk2 = document.getElementById("360");
-let dunk3 = document.getElementById("underleg");
+// let dunk1 = document.getElementById("windmill");
+// let dunk2 = document.getElementById("360");
+// let dunk3 = document.getElementById("underleg");
 
-  let myDunks = [dunk1, dunk2, dunk3];
+//   let myDunks = [dunk1, dunk2, dunk3];
   
-  function shoot() {
+//   function shoot() {
         
-  }
+//   }
+
+let vidArray = new Array();
+
+let outputDiv = document.getElementById('dunkVids');
+
+function loadVid(gameShot){
+    let select = gameShot.shotSelection.selectedIndex;
+    let selectVid = gameShot.shotSelection[select].text;
+
+    alert('selection working');
+
+}
+function vidOptions(url, title) {
+    this.url = url;
+    this.title = title;
+
+    this.getVid = function(){return this.url + this.title;}
+
+}
